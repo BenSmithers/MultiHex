76cb05285ced0fd708aaa755015f9077291fc8fb
BoggleHead <brsmithe@ucsc.edu>
2019-11-11 22:58:12 -0600

    implemented special hexes

--- Python_v/special_hexes.py
+++ Python_v/special_hexes.py
@@ -1,36 +1,54 @@
 from hex import Hex
+from point import Point
 
-class Overland(Hex):
-    pass
+default_p = Point(0.0,0.0)
 
 class Ocean(Hex):
-    self._temperature_base = 0.5
-    self._humidity_base    = 1.0
-    self._altitude_base    = 0.0
-    self._is_land          = False
-    pass
+    def __init__(self, center=default_p, radius=1.0 ):
+        Hex.__init__(self, center, radius)
+        self.fill = 'powderblue'
+        self._temperature_base = 0.5
+        self._humidity_base    = 1.0
+        self._altitude_base    = 0.0
+        self._is_land          = False
+        
 
 class Grassland(Hex):
-    pass
+    def __init__(self, center=default_p, radius=1.0 ):
+        Hex.__init__(self, center, radius )
+        self.fill = 'green3'
+    
 
 class Forest(Hex):
-    pass
+    def __init__(self, center=default_p, radius=1.0 ):
+        Hex.__init__(self, center, radius )
+        self.fill = 'forest green'
+    
 
 class Mountain(Hex):
-    pass
+    def __init__(self, center=default_p, radius=1.0 ):
+        Hex.__init__(self, center, radius )
+        self.fill = 'thistle4'
+    
 
 class Arctic(Hex):
-    self._temperature_base = 0.0
-    self._humidity_base    = 0.0
-    self._altitude_base    = 0.0
-    self._is_land          = True
-    pass
+    def __init__(self, center=default_p, radius=1.0 ):
+        Hex.__init__(self, center, radius )
+        self.fill = 'snow'
+        self._temperature_base = 0.0
+        self._humidity_base    = 0.0
+        self._altitude_base    = 0.0
+        self._is_land          = True
+    
 
 class Desert(Hex):
-    self._temperature_base = 1.0
-    self._humidity_base    = 0.0
-    self._altitude_base    = 0.0
-    self._is_land          = True
-    pass
+    def __init__(self, center=default_p, radius=1.0 ):
+        Hex.__init__(self, center, radius )
+        self.fill = 'goldenrod'
+        self._temperature_base = 1.0
+        self._humidity_base    = 0.0
+        self._altitude_base    = 0.0
+        self._is_land          = True
+
 
 
