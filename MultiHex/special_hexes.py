de54d398e84cadbca4c78a08e8b796b385d7af28
Boggle-Head <b.smithers.actual@gmail.com>
2019-11-24 17:55:39 -0600

    consolidated colors under one flag

--- MultiHex/special_hexes.py
+++ MultiHex/special_hexes.py
@@ -3,9 +3,22 @@ from MultiHex.point import Point
 
 default_p = Point(0.0,0.0)
 
+class hcolor:
+    def __init__(self):
+        self.ocean = (91,201,192)
+        self.grass = (149,207,68)
+        self.fores = (36, 94, 25)
+        self.arcti = (171,224,224)
+        self.mount = (158,140,96)
+        self.ridge = (99,88,60)
+        self.deser = (230,178,110)
+        self.rainf = (22,77,57)
+        self.savan = (170, 186, 87)
+colors = hcolor()
+
 def Ocean_Hex(center, radius):
     temp = Hex(center, radius)
-    temp.fill = (91,201,192)
+    temp.fill = colors.ocean
     temp._temperature_base = 0.5
     temp._rainfall_base    = 1.0
     temp._biodiversity     = 1.0
@@ -15,24 +28,24 @@ def Ocean_Hex(center, radius):
 
 def Grassland_Hex(center,radius):
     temp = Hex( center, radius )
-    temp.fill = (149,207,68)
+    temp.fill = colors.grass
     temp._is_land = True
     return(temp)
 
 def Forest_Hex(center,radius):
     temp = Hex( center, radius )
-    temp.fill = (27,110,11)
+    temp.fill = colors.fores
     return(temp)
 
 def Mountain_Hex(center,radius):
     temp = Hex( center, radius )
-    temp.fill = (158,140,96)
+    temp.fill = colors.mount
     temp.genkey = '11000000'
     return(temp)
 
 def Arctic_Hex(center,radius):
     temp = Hex( center, radius )
-    temp.fill = (171,224,224)
+    temp.fill = colors.arcti
     temp._temperature_base = 0.0
     temp._rainfall_base    = 0.0
     temp._altitude_base    = 0.0
@@ -41,11 +54,12 @@ def Arctic_Hex(center,radius):
 
 def Desert_Hex(center,radius):
     temp = Hex( center, radius )
-    temp.fill = (230,178,110)
+    temp.fill = colors.deser
     temp._temperature_base = 1.0
     temp._rainfall_base    = 0.0
     temp._altitude_base    = 0.0
     temp._is_land          = True
     return(temp)
+
 
 
