9f97725a7ad3347bda51d2053fe5da9d0bb01a6d
BenSmithers <b.smithers.actual@gmail.com>
2020-02-01 23:55:01 -0600

    Began implementation of sunrise algorithm

--- MultiHex/clock.py
+++ MultiHex/clock.py
@@ -3,13 +3,13 @@
 # in our fictional universe there are no timezones... no leap anything.
 # it's a perfect clockwork universe. Hurray
 
-# prefer the numpy version, but the default one is okay 
-try:
-    from numpy import floor
-except ImportError:
-    from math import floor
+
+from math import pi, abs, floor
+from math import cos, sin, arcsin, sqrt
 
 months = {}
+
+degrees = pi/180.
 
 minutes_in_hour = 60
 hours_in_day    = 24
@@ -35,6 +35,34 @@ month_list = [  "January",
 
 assert( len(month_list) == months_in_year )
 
+class Time:
+    """
+    Used to pass times around 
+    """
+
+    def __init__( self, hour, minute ):
+        assert( minute>=0 and minutes<minutes_in_hour)
+        assert( hour>=0 and hour<hours_in_day )
+
+        morning = ( hour < hours_in_day/2  )
+
+        self.hour = hour 
+        self.minute = minute
+
+    def __str__(self):
+        if morning:
+            if self.hour == 0:
+                out = "12:{} AM".format( self.minute )
+            else:
+                out = "{}:{} AM".format( self.hour, self.minute )
+        else: 
+            if self.hour == 12:
+                out = "12:{} PM".format( self.minute)
+            else:
+                out = "{}:{} PM".format( self.hour - hours_in_day/2 , self.minute)
+
+        return( out )
+
 class Clock:
     """
     Simple clock class to keep track of the time 
@@ -42,17 +70,13 @@ class Clock:
 
     def __init__(self):
         # this counts the minutes through an entire year
-        self._minutes = 0
-        
-        # this counts the year
-        self._cyear     = False
-        self._year      = 0
-        self._cmonth    = False
-        self._month     = 0
-        self._cday      = False
-        self._day       = 0
-        self._chour     = False
+        self._minute    = 0
         self._hour      = 0
+        self._day       = 0
+        self._month     = 0
+        self._year      = 0
+
+        self._axial_tilt = 17*degrees
 
     def month_str(self):
         """
@@ -60,28 +84,16 @@ class Clock:
         """
         if type(self.month())!=int:
             raise TypeError("That's not right... month is type {}".format(type(self.month)))
-
-        n_month = self.month()
         
-        return( month_list[n_month] )
+        return( month_list[self._month] )
 
     def year(self):
-        if self._cyear:
-            pass
-        else:
-            self._year = int(floor(( self._minutes )/minutes_in_year))
-            self._cyear = True
-        return( self._year)
+        return( self._year )
 
     def month(self):
         """
         Given the minutes elapsed in the year so far, return what month it is
         """
-        if self._cmonth:
-            pass
-        else:
-            self._month = int(floor((self._minutes - self.year()*minutes_in_year) / minutes_in_month ))
-            self._cmonth = True
         return( self._month )
 
 
@@ -89,22 +101,12 @@ class Clock:
         """
         Returns the day of the month as an int.
         """
-        if self._cday:
-            pass
-        else:
-            self._day   =  int(floor((self._minutes - self.year()*minutes_in_year - self.month()*minutes_in_month) / minutes_in_day ))
-            self._cday = True
         return( self._day )
 
     def hour(self):
         """
         Returns the hour of the day (24 hour format) as an int. 
         """
-        if self._chour:
-            pass
-        else:
-            self._hour = int(floor( (self._minutes -self.year()*minutes_in_year - self.month()*minutes_in_month - self.day()*minutes_in_day) / minutes_in_hour ))
-            self._chour = True
         return( self._hour )
 
     def minute(self):
@@ -112,60 +114,96 @@ class Clock:
         Returns the current minute of the hour as an int. 
         """
         
-        n_minutes =  self._minutes - self.year()*minutes_in_year - self.month()*minutes_in_month - self.day()*minutes_in_day - self.hour()*minutes_in_hour 
-        return(n_minutes)
-    
+        return(self._minute)
 
-    def set_month(self, month): 
-        """
-        Turns the clock forward until the specified month. Month must be specified as an integer.
-        """
-        if type(month)!=int:
-            raise TypeError("Month must be of type {}, reveived {}.".format(int, type(month)))
-        difference = month -self.month()
-        if difference<0:
-            difference += months_in_year
+    def _month_step(self, monthts):
+        self._month = months + self._month
 
-        self.time_step(months=difference )
+        while self._month >= months_in_year:
+            self._year += 1
+            self._month -= months_in_year
 
-    def set_day(self, day ):
-        """
-        Turns the clock forward to the provided day. Day must be an int.
-        """
-        if type(day)!=int:
-            raise TypeError("Object 'day' must be of type {}, received {}".format(int, type(day)))
-        difference = day-self.day()
-        if difference<0:
-            difference += days_in_month
+    def _day_step(self, days):
+        self._day += days
 
-        self.time_step( days=difference )
+        while self._day >= days_in_month:
+            self._month_step( 1 )
+            self._day-= days_in_month
+
+    def _hour_step(self, hours):
+        self._hour += hours
+
+        while self._hour >= hours_in_day:
+            self._day_step(1)
+            self._hour -= hours_in_day
 
 
-    def set_year(self, year ):
+    def _minute_step(self, minutes):
+        self._minute += minutes
+        
+        while self._minute >= minutes_in_hour:
+            self._hour_step(1)
+            self._minute -= minutes_in_hour
+
+    def minute_of_day(self):
+        return( self._minute + self._hour*minutes_in_hour )
+
+    def days_of_year(self):
+        return( self._day + self.month*months_in_year )
+
+    def get_next_suntime(self, latitude):
         """
-        Sets the year to specified year. Year must be provided as an int. 
+        Implements sunrise algorithm. Returns tuple of this day's (sunrise, sunset).
+
+        @param latitude     - latitude of point for sunrise/set. Should be in RADIANS
         """
-        if type(year)!=int:
-            raise TypeError("Object 'year' must be of type {}, received {}".format(int, type(year)))
-        self.time_step( years = year )
+        assert( abs(latitude)<=pi )
+
+        # representative of angle around sun
+        alpha = 2*pi*float(self.days_of_year())/(days_in_month*months_in_year)
+        
+        # calculate the constants
+        A = cos(alpha)*sin(latitude)
+        B = sin(alpha)*cos(self._axial_tilt)*sin(latitude)
+        C = sin(alpha)*sin(self._axial_tilt)*cos(latitude)
+
+        under_root = 4*(C**2)*(B**2) + (A**2 + B**2)*(A**2 - C**2)
+        
+        # check if it's deep winter or summer 
+        if under_root<0:
+            if (latitude > 0) and ((pi/2)<alpha or alpha<(3*pi/2)):
+                return("Sunny") # sunny 
+            if (latitude < 0) and not ((pi/2)<alpha or alpha<(3*pi/2)):
+                return() # sunny
+            
+            return() # dark
+
+        soln_1 = arcsin( (2*C*B+sqrt(under_root))/( 2*A**2 + 2*B**2 ) )
+        soln_2 = arcsin( (2*C*B-sqrt(under_root))/( 2*A**2 + 2*B**2 ) )
+
+        if (alpha>pi/2) and (alpha<3*pi/2):
+            if latitude>0:
+                pass
+            else:
+                pass
+        else:
+            pass
 
 
     def time_step(self, minutes=0, hours=0, days=0, months=0, years=0):
         """
         Move the current time forward by some number of minutes (or optionally years, months, etc)
         """
+        if minutes!=0:
+            self._minute_step( minutes )
+        if hours!=0:
+            self._hour_step( hours)
+        if days!=0:
+            self._day_step(days)
+        if months!=0:
+            self._month_step(months)
 
-        self._minutes += years*minutes_in_year
-        self._minutes += months*minutes_in_month
-        self._minutes += days*minutes_in_day
-        self._minutes += hours*minutes_in_hour
-        self._minutes += minutes
-        
-        # these are no longer accurate! 
-        self._cyear = False
-        self._cmonth = False
-        self._cday = False
-        self._chour = False
+        self._year += years
 
     def get_moon_phase(self):
         """
