400af4a68c54333c07198a38a71845ba69c904a4
BenSmithers <b.smithers.actual@gmail.com>
2020-02-03 22:37:49 -0600

    Multiple additions! Refined time object and its printouts. Added functions: change axial tilt, skip to a given time, skip to a holiday, skip to any day, skip to a certain time, skip to a certain lunar phase, get the amount of time until a holiday, get the height of the sun in the sky.

--- MultiHex/clock.py
+++ MultiHex/clock.py
@@ -6,13 +6,13 @@
 from math import pi, floor
 from math import cos, sin, sqrt
 
-from numpy import arcsin
+from numpy import arcsin, arccos
 
 degrees = pi/180.
 
 minutes_in_hour = 60
 hours_in_day    = 24
-days_in_month   = 30
+days_in_month   = 35
 months_in_year  = 12
 
 minutes_in_day   = minutes_in_hour*hours_in_day
@@ -31,6 +31,8 @@ month_list = [ "January",
             "October",
             "November",
             "December" ]
+assert( len(month_list) == months_in_year )
+
 
 day_list = [ "Monday",
           "Tuesday",
@@ -40,6 +42,8 @@ day_list = [ "Monday",
           "Saturday",
           "Sunday"]
 
+
+
 phases = {  "New":0.0,
             "Waxing Crescent":0.3,
             "Waxing Gibbous":0.6,
@@ -47,14 +51,13 @@ phases = {  "New":0.0,
             "Wanning Gibbous":0.6,
             "Wanning Crescent":0.3}
 
-assert( len(month_list) == months_in_year )
 
 class Time:
     """
     Used to pass around and properly format the times
     """
 
-    def __init__( self, hour=0, minute=0, month=0, day = 0,year = 0 ):
+    def __init__( self , hour=0, minute=0, month=0, day = 0,year = 0,date=True):
         if not ( minute>=0 ):
             raise ValueError("Invalid number of minutes {}".format(minute))
         if not (hour>=0 and hour<hours_in_day):
@@ -66,6 +69,8 @@ class Time:
         self._month     = month
         self._day       = day
         self._year      = year
+
+        self.date      = date
 
         while self._minute > minutes_in_hour:
             self._hour += 1
@@ -93,6 +98,7 @@ class Time:
     def minute(self):
 
         return(self._minute)
+
 
     def month_str(self):
         """
@@ -168,19 +174,34 @@ class Time:
         self._year += years
 
     def __str__(self):
-        self.morning = ( self.hour < hours_in_day/2  )
-        if self.morning:
-            if self.hour == 0:
-                out = "12:{:02d} AM".format( self.minute )
-            else:
-                out = "{}:{:02d} AM".format( self.hour, self.minute )
-        else: 
-            if self.hour == 12:
-                out = "12:{:02d} PM".format( self.minute)
-            else:
-                out = "{}:{:02d} PM".format( int(self.hour - hours_in_day/2 ), self.minute)
+        if self.date:
+            self.morning = ( self.hour < hours_in_day/2  )
+            if self.morning:
+                if self.hour == 0:
+                    out = "12:{:02d} AM".format( self.minute )
+                else:
+                    out = "{}:{:02d} AM".format( self.hour, self.minute )
+            else: 
+                if self.hour == 12:
+                    out = "12:{:02d} PM".format( self.minute)
+                else:
+                    out = "{}:{:02d} PM".format( int(self.hour - hours_in_day/2 ), self.minute)
 
-        return("{} {}, {}, at {}".format(self.month_str(), self._day, self._year, out))
+            return("{} {}, {}, at {}".format(self.month_str(), self._day, self._year, out))
+        else:
+            out = ""
+            if self._year!=0:
+                out+="{} years, ".format(self._year)
+            if self._month!=0:
+                out+="{} months, ".format(self._month)
+            if self._day!=0:
+                out+="{} days, ".format(self._day)
+            if self._hour!=0:
+                out+="{} hours, ".format(self._hour)
+            if out!="":
+                out+="and "
+            out += "{} minutes".format(self._minute)
+            return(out)
 
     def __repr__(self):
         return("<Time Object: {}>".format(self))
@@ -260,7 +281,7 @@ class Time:
                         else: # either it's greater than or equal to
                             return( False )
     def __gt__(self, other):
-        return( (not self.__it__(other)) and (self.minute!=other.minute))
+        return( (not self.__lt__(other)) and (self.minute!=other.minute))
 
 class Clock:
     """
@@ -276,26 +297,82 @@ class Clock:
         self._coax = cos(self._axial_tilt)
         self._siax = sin(self._axial_tilt)
 
+        self._lunar_offset = 3 
+        self._lunar_length = 28
+
         self._holidays = { "Winter_Solstice":Time(month=0),
                     "Spring_Equinox":Time(month=3),
                     "Summer_Solstice":Time(month=6),
                     "Autumnal_Equinox":Time(month=9) }
 
-    def go_to_holiday(self, holiday):
-        if not isinstance(name, str):
-            raise TypeError("Expected type {}, got {}".format(str, type(name)))
+    def change_axial_tilt(self, new_tilt):
+        """
+        Changes the axial tilt of the planet to the provided tilt
+
+        @param new_tilt     - float, radian ( 0 , pi )
+        """
+        if not isinstance( new_tilt, float):
+            raise TypeError("Expected {}, got {}".format(float, type(new_tilt)))
+        if not (new_tilt>0 and new_tilt<pi):
+            raise ValueError("Invalid tilt {}".format(new_tilt))
+
+        self._axial_tilt = new_tilt
+
+    def get_tilt(self):
+        """
+        Returns the axial tilt of the planet
+        """
+        return( self._axial_tilt )
+
+    def skip_to_holiday(self, holiday):
+        """
+        Skips the clock to the registered holiday with specified key (name)
+        """
+        if not isinstance(holiday, str):
+            raise TypeError("Expected type {}, got {}".format(str, type(holiday)))
         if holiday not in self._holidays:
             raise ValueError("{} not a known holiday".format(holiday))
 
         when = self._holidays[holiday]
-        when._year = self._time.year
+        self.skip_to_day( when )
 
-        if when < self._time:
-            when._year = when._year + 1
+    def skip_to_time(self, what_time):
+        """
+        Dials the clock /forward/ to a specific time of day
+        """
+        if not isinstance(what_time, Time):
+            raise TypeError("Expected {}, got {}".format(Time, type(what_time)))
 
-        self.skip_to_time( when )
+        what_time._year = self._time.year
+        what_time._month = self._time.month
+        what_time._day   = self._time.day
+
+        if what_time < self._time:
+            what_time._day = what_time._day + 1
+
+        self.skip_to( what_time )
+
+    def skip_to_day( self, day ):
+        """
+        Skips to a specific day of the year. 
+
+        @param day  - A Time object. Its year, minute, and hour are ignored. Only the day and month are used
+        """
+        if not isinstance(day, Time):
+            raise TypeError("Expected {}, got {}".format(Time, type(day)))
+
+        day._year = self._time.year
+        day._minute= 0
+        day._hour  = 0
+        if day < self._time:
+            day._year = day._year + 1
+
+        self.skip_to( day )
 
     def add_holiday( self, time_obj, name):
+        """
+        Registers a _federal_ holiday with the Clock
+        """
         if not isinstance(name, str):
             raise TypeError("Expected type {}, got {}".format(str, type(name)))
         if not isinstance(time_obj, Time):
@@ -321,13 +398,32 @@ class Clock:
         hour_shift = int( hours_in_day*(long/(2*pi)) )
         return( self._time + Time(hour=hour_shift) )
 
-    def _time_to_phase(self, phase):
+    def skip_to_phase(self, phase):
         if not isinstance(phase, str):
             raise TypeError("Expected {}, got {}".format(str, type(phase)))
 
 
+        # how many days through a cycle is the target phase?
+        how_many = int(list(phases.keys()).index(phase)*float(self._lunar_length)/len(phases.keys()))
 
-        how_many = index(phase)*length
+        # how many days through the cycle are we?
+        total_days = self._time.day + self._time.month*days_in_month + self._time.year*days_in_month*months_in_year
+        days_through_cycle = (total_days - self._lunar_offset) % self._lunar_length 
+        
+        if days_through_cycle > how_many:
+            # if we're passed it, step into the next cycle
+            days_to_skip = self._lunar_length - (days_through_cycle - how_many)
+        else:
+            # if we haven't reached it, go to it
+            days_to_skip = how_many - days_through_cycle
+
+        # sted to one day before the cycle 
+        if days_to_skip > 1:
+            self.time_step(Time( day = (days_to_skip - 1  )))
+
+        # default time is midnight
+        # skip to then
+        self.skip_to_time( Time() )
 
     def get_moon_phase(self):
         """
@@ -336,30 +432,74 @@ class Clock:
         Moon phase simplified to be exactly 28 days long
         """
 
-        # just so the moon phase isn't perfectly aligned with the weeks! 
-        offset = 3  # days
-        length = 28 # days
-
         total_days = self._time.day + self._time.month*days_in_month + self._time.year*days_in_month*months_in_year
-        days_through_cycle = (total_days - offset) % length 
+        days_through_cycle = (total_days - self._lunar_offset) % self._lunar_length 
         
-        percent_through = float( days_through_cycle )/length
+        percent_through = float( days_through_cycle )/self._lunar_length
         which_part = int( percent_through * len(phases.keys()) )
         
         return( list(phases.keys())[which_part] )
 
     def get_base_moon_light(self):
+        """
+        Returns the amount of light given off by the moon right now (regardless of where it is in the sky) 
+        """
         return(phases[self.get_moon_phase()])
 
          
-    def skip_to_time(self, new_time):
+    def skip_to(self, new_time):
+        """
+        Turns the clock forward to the given Time
+        """
         if not isinstance(new_time, Time):
             raise TypeError("Expected object of type {}, got {}.".format(Time, type(new_time)))
 
+        if new_time < self._time:
+            raise ValueError("{} is in the past. It's {}".format(new_time, self._time))
+
         lapse = new_time - self._time
-        self._time.time_step( lapse )
+
+        self._time.time_step(lapse.minute, lapse.hour, lapse.day, lapse.month, lapse.year)
+
+    def get_time_to_holiday(self, holiday):
+        """
+        Returns the amount of time, as a Time object, to the next holiday 
+        """
+        if not isinstance(holiday, str):
+            raise TypeError("Expected type {}, got {}".format(str, type(holiday)))
+        if holiday not in self._holidays:
+            raise ValueError("{} not a known holiday".format(holiday))
+
+        when = self._holidays[holiday]
+        when._year = self._time.year
+
+        if when < self._time:
+            when._year = when._year + 1
+
+        time_to = when - self._time
+        time_to.date = False
+
+        return(time_to)
+
+    def get_sun_angle(self, lat, long):
+        """
+        Returns the height of the sun above the horizon, in radians
+        """
+        ll = self.get_current_light_level(lat, long)
+
+        if ll < 0:
+            return()
+        else:
+            return( pi - arccos( ll ))
 
     def get_light_level(self, time_minutes, lat, long):
+        """
+        Returns the light level (-1 to 1) at the given latitude, longitude, and point in time
+
+        < -0.1 is nighttime 
+        -0.1 < ll < 0.1 is twilight
+        >0.1 is day time
+        """
         yr_freq = 2*pi/minutes_in_year
         dy_freq = 2*pi/minutes_in_day
 
@@ -381,15 +521,26 @@ class Clock:
             return(light_level)
 
     def time_step(self, amount):
+        """
+        Turns the clock forward by an amount of time.
+
+        amount should be of type Time
+        """
         #self._time.time_step( minutes, hours, days, months, years)
         if not isinstance(amount, Time):
             raise TypeError("Expected object of type {}, got {}.".format(Time, type(amount)))
         self._time.time_step( amount.minute, amount.hour, amount.day, amount.month, amount.year)
 
     def get_current_light_level(self, lat, lon):
+        """
+        Get the latitude and longitude at the current moment in time 
+        """
         return( self.get_light_level( self.get_time_in_minutes(), lat,lon))
 
     def get_next_suntime( self, lat, lon):
+        """
+        Returns the time of the next sunrise or sunset 
+        """
         ll = self.get_current_light_level(lat, lon)
         time = self.get_time_in_minutes()
         stepped = 0
@@ -411,12 +562,13 @@ class Clock:
     def get_moon_visibility(self, long, current=True, time=None):
 
         if current:
-            time = self.get_time_in_minutes()
+            time_minutes = self.get_time_in_minutes()
         else:
             if time is None:
                 raise ValueError("If not asking for current, must provide time")
             if not isinstance(time, int):
                 raise TypeError("Expected type {}, got {}".format(int, type(time)))
+            time_minutes = time
 
         yr_freq = 2*pi/minutes_in_year
         dy_freq = 2*pi/minutes_in_day
@@ -429,8 +581,8 @@ class Clock:
         cos_omgom   = cos(time_minutes*(-1*yr_freq + dy_freq))
         sin_omgom   = sin(time_minutes*(-1*yr_freq + dy_freq))
 
-        offset = 3*minutes_in_day
-        length = 28*minutes_in_day
+        offset = self._lunar_offset*minutes_in_day
+        length = self._lunar_length*minutes_in_day
 
         ang_velocity = 2*pi/length
         phase = 2*pi*(float(offset)/length)
@@ -439,6 +591,11 @@ class Clock:
 
         moon     = cos(ang_velocity*time_minutes + phase)*(cos_omgom*cos_lon*self._coax - sin_omgom*sin_lon*self._coax)
         moon    += sin(ang_velocity*time_minutes + phase)*(sin_omgom*cos_lon + cos_omgom*sin_lon )
+
+        if moon<0:
+            return(False)
+        else:
+            return(True)
 
     def get_time_in_minutes(self):
         time = self._time.minute
