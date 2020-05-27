e55f17da5d20f901680b8d54a544763beba02f70
BenSmithers <b.smithers.actual@gmail.com>
2020-05-27 17:37:53 -0500

    Fundamental reworking to how the GUIs work. There will only ever be one MainWindow now. Switching between the editors is done by creating a new object which adds to the main GUI and creates linkages between the new elements and the main map elements. Then, these are all cleared out and deleted when switching to a new editor

--- MultiHex/guis/main_gui.py
+++ MultiHex/guis/main_gui.py
@@ -19,19 +19,19 @@ class main_gui(object):
         self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
         self.horizontalLayout.setObjectName("horizontalLayout")
 
-        # the tool pane! Icons should be added to this
+        # the tool pane! Icons should be added to this (by default it's empty)
         self.toolPane = QtWidgets.QVBoxLayout()
         self.toolPane.setObjectName("toolPane")
         spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.toolPane.addItem(spacerItem)
+        self.horizontalLayout.addLayout(self.toolPane)
 
         # define the screen
-        # self.horizontalLayout.addLayout(self.verticalLayout)
         self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget) # <--- this is the screen
         self.graphicsView.setObjectName("graphicsView") 
         self.horizontalLayout.addWidget(self.graphicsView)
 
-        # this is the contextPane
+        # this is the contextPane - it's where all the panels go on the right
         self.contextPane = QtWidgets.QToolBox(self.centralwidget)
         self.contextPane.setMinimumSize(QtCore.QSize(250,0))
         self.contextPane.setMaximumSize(QtCore.QSize(250, 16777215))
@@ -49,12 +49,9 @@ class main_gui(object):
         self.menubar.setObjectName("menubar")
         self.menuFile = QtWidgets.QMenu(self.menubar)
         self.menuFile.setObjectName("menuFile")
-        self.menuHelp = QtWidgets.QMenu(self.menubar)
-        self.menuHelp.setObjectName("menuHelp")
-        self.menuHeatmaps = QtWidgets.QMenu(self.menubar)
-        self.menuHeatmaps.setObjectName("menuHeatmaps")
-        self.menuView = QtWidgets.QMenu(self.menubar)
-        self.menuView.setObjectName("menuView")
+        self.menuEditor = QtWidgets.QMenu(self.menubar)
+        self.menuEditor.setObjectName("menuEditor")
+        
         MainWindow.setMenuBar(self.menubar)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -69,24 +66,14 @@ class main_gui(object):
         self.actionSave_As.setObjectName("actionSave_As")
         self.actionQuit = QtWidgets.QAction(MainWindow)
         self.actionQuit.setObjectName("actionQuit")
-        self.actionAbout_MultiHex = QtWidgets.QAction(MainWindow)
-        self.actionAbout_MultiHex.setObjectName("actionAbout_MultiHex")
-        self.actionTemperature = QtWidgets.QAction(MainWindow)
-        self.actionTemperature.setCheckable(False)
-        self.actionTemperature.setObjectName("actionTemperature")
-        self.actionAltitude = QtWidgets.QAction(MainWindow)
-        self.actionAltitude.setObjectName("actionAltitude")
-        self.actionRainfall = QtWidgets.QAction(MainWindow)
-        self.actionRainfall.setObjectName("actionRainfall")
-        self.actionBiome_Names = QtWidgets.QAction(MainWindow)
-        self.actionBiome_Names.setCheckable(True)
-        self.actionBiome_Names.setObjectName("actionBiome_Names")
-        self.actionBiome_Borders = QtWidgets.QAction(MainWindow)
-        self.actionBiome_Borders.setCheckable(True)
-        self.actionBiome_Borders.setObjectName("actionBiome_Borders")
-        self.actionRivers = QtWidgets.QAction(MainWindow)
-        self.actionRivers.setCheckable(True)
-        self.actionRivers.setObjectName("actionRivers")
+
+        self.actionTerrainEditor = QtWidgets.QAction(MainWindow)
+        self.actionTerrainEditor.setCheckable(True)
+        self.actionTerrainEditor.setObjectName("actionTerrainEditor")
+        self.actionCivEditor = QtWidgets.QAction(MainWindow)
+        self.actionCivEditor.setCheckable(True)
+        self.actionCivEditor.setObjectName("actionCivEditor")
+        
         self.menuFile.addAction(self.actionNew)
         self.menuFile.addAction(self.actionOpen)
         self.menuFile.addSeparator()
@@ -94,15 +81,23 @@ class main_gui(object):
         self.menuFile.addAction(self.actionSave)
         self.menuFile.addSeparator()
         self.menuFile.addAction(self.actionQuit)
-        self.menuHelp.addAction(self.actionAbout_MultiHex)
-        self.menuHeatmaps.addAction(self.actionAltitude)
-        self.menuHeatmaps.addAction(self.actionRainfall)
-        self.menuHeatmaps.addAction(self.actionTemperature)
-        self.menuView.addAction(self.actionBiome_Names)
-        self.menuView.addAction(self.actionBiome_Borders)
-        self.menuView.addSeparator()
-        self.menuView.addAction(self.actionRivers)
+
+        self.menuEditor.addAction(self.actionTerrainEditor)
+        self.menuEditor.addAction(self.actionCivEditor)
+
         self.menubar.addAction(self.menuFile.menuAction())
-        self.menubar.addAction(self.menuView.menuAction())
-        self.menubar.addAction(self.menuHeatmaps.menuAction())
-        self.menubar.addAction(self.menuHelp.menuAction())
+        self.menuFile.setTitle(QtCore.QCoreApplication.translate("MainWinbdow","File"))
+        self.actionNew.setText(QtCore.QCoreApplication.translate("MainWindow", "New"))
+        self.actionOpen.setText(QtCore.QCoreApplication.translate("MainWindow", "Open"))
+        self.actionSave.setText(QtCore.QCoreApplication.translate("MainWindow", "Save"))
+        self.actionSave_As.setText(QtCore.QCoreApplication.translate("MainWindow", "Save As"))
+        self.actionQuit.setText(QtCore.QCoreApplication.translate("MainWindow", "Quit"))
+
+        self.menubar.addAction(self.menuEditor.menuAction())
+        self.menuEditor.setTitle(QtCore.QCoreApplication.translate("MainWinbdow","Editor"))
+        self.actionTerrainEditor.setText(QtCore.QCoreApplication.translate("MainWinbdow","Terrain Editor"))
+        self.actionCivEditor.setText(QtCore.QCoreApplication.translate("MainWinbdow","Civilization Editor"))
+
+
+
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
