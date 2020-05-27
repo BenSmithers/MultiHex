c4f9714d072b01cf16c1ad57120dfc278035e3a8
BenSmithers <b.smithers.actual@gmail.com>
2020-05-27 17:38:20 -0500

    Added much more of the general functionality

--- MultiHex/main_file.py
+++ MultiHex/main_file.py
@@ -3,16 +3,21 @@ from PyQt5.QtWidgets import QMainWindow, QFileDialog, QDialog, QApplication, QWi
 
 import os
 import sys 
+import json
 
 # standard GUI
 from MultiHex.guis.main_gui import main_gui
 
 from MultiHex.core import Hexmap, save_map, load_map
 from MultiHex.tools import basic_tool
-from MultiHex.map_types.overland import OEntity_Brush, OHex_Brush, Road_Brush, County_Brush, Nation_Brush, Nation, Biome_Brush, River_Brush, ol_clicker_control
+from MultiHex.map_types.overland import OEntity_Brush, OHex_Brush, Road_Brush, County_Brush, Nation_Brush, Nation, Biome_Brush, River_Brush, ol_clicker_control, Detail_Brush
 from MultiHex.about_class import about_dialog
-from MultiHex.load_new import new_load_dialog
-from MultiHex.generator.util import get_tileset_params
+from MultiHex.generator.util import get_tileset_params, create_name, Climatizer
+
+# import other guis! 
+from MultiHex.guis.terrain_editor_gui import terrain_ui
+from MultiHex.guis.civ_gui import civ_ui
+from MultiHex.guis.new_load_gui import new_load_gui
 
 class main_window(QMainWindow):
     def __init__(self,parent=None):
@@ -31,6 +36,8 @@ class main_window(QMainWindow):
         self.county_control = County_Brush( self )
         self.county_control.small_font = False
         self.nation_control = Nation_Brush(self)
+        self.detail_control = Detail_Brush(self)
+        self.climatizer = Climatizer( "standard" )
 
         # Allow the graphics view to follow the mouse when it isn't being clicked, and associate the clicker control with the ui 
         self.scene = ol_clicker_control( self.ui.graphicsView, self )
@@ -41,41 +48,76 @@ class main_window(QMainWindow):
         # any mouse commands will just be thrown in the trash 
         self.scene._active = basic_tool(self)
         
-
         # set up the action bar! 
-        """
         self.ui.actionQuit.triggered.connect( self.quit )
         self.ui.actionSave.triggered.connect( self.save_map )
         self.ui.actionSave_As.triggered.connect( self.save_as)
-        self.ui.actionBiome_Borders.triggered.connect( self.action_biome_bord )
-        self.ui.actionCounty_Borders.triggered.connect(self.action_county_bord )
-        self.ui.actionBiome_Names.triggered.connect( self.action_biome_names )
-        self.ui.actionCounty_Names.triggered.connect( self.action_county_names )
-        self.ui.actionTowns.triggered.connect( self.action_towns )
-        self.ui.actionLocations.triggered.connect( self.action_locations )
-        self.ui.actionAbout_MultiHex.triggered.connect( self.actionAbout )
-        """
+        self.ui.actionOpen.triggered.connect( self.open )
+        self.ui.actionNew.triggered.connect(self.new)
+        self.ui.actionTerrainEditor.triggered.connect(self.switch_to_terrain)
+        self.ui.actionCivEditor.triggered.connect(self.switch_to_civilization)
+
+        self._ui_clear_function = None
+        self.extra_ui = None
+
+        loc = os.path.join(os.path.dirname(__file__),'resources', 'tilesets.json')
+        file_object = open( loc, 'r')
+        self.config = json.load( file_object )
+        file_object.close()
+        self.params = []
+
+    def smart_ui_chooser(self):
+        self.switch_to_terrain()
+
+    def switch_to_terrain(self):
+        self.scene.drop()
+        if self._ui_clear_function is not None:
+            self._ui_clear_function(self.ui)
+        self.extra_ui = terrain_ui(self)
+        self.ui.actionCivEditor.setChecked(False)
+        self.ui.actionTerrainEditor.setChecked(True)
+        self._ui_clear_function = self.extra_ui.clear_ui
+
+
+    def switch_to_civilization(self):
+        self.scene.drop()
+        if self._ui_clear_function is not None:
+            self._ui_clear_function(self.ui)
+
+        self.extra_ui = civ_ui(self)
+        self.ui.actionCivEditor.setChecked(True)
+        self.ui.actionTerrainEditor.setChecked(False)
+        self._ui_clear_function = self.extra_ui.clear_ui
 
     def show(self):
         QMainWindow.show(self)
         # this either has MH load a map or make a new map 
         self.main_map = None
-        dialog = new_load_dialog(self)
-        dialog.setAttribute(QtCore.Qt.WA_DeleteOnClose)
-        dialog.exec_()
+        while self.main_map is None:
+            dialog = new_load_dialog(self)
+            dialog.setAttribute(QtCore.Qt.WA_DeleteOnClose)
+            dialog.exec_()
 
     def new(self):
+        pass
+
+    def open(self):
         pass
 
     def load(self):
         """
         Called when we want to load a new map
         """
-        filename = QFileDialog.getOpenFileName(None, 'Open HexMap', os.path.join(os.path.dirname(__file__), "saves"), 'HexMaps (*.hexmap)')[0]
+        self.filename = QFileDialog.getOpenFileName(None, 'Open HexMap', os.path.join(os.path.dirname(__file__), "saves"), 'HexMaps (*.hexmap)')[0]
+        if self.filename is None:
+            return
+        elif not os.path.exists(self.filename): 
+            # either the user chose something that doesn't exist, or they canceled 
+            return
+
         self.ui.graphicsView.update()
-        self.main_map = load_map( filename )
-        self.file_name = filename 
-        
+        self.main_map = load_map( self.filename )
+        self.smart_ui_chooser()
         self.params = get_tileset_params( self.main_map.tileset )
 
         print("Drawing hexes... ", end='')
@@ -92,6 +134,7 @@ class main_window(QMainWindow):
         self._redraw_roads()
         self._redraw_counties()
         print("done")
+        
 
     def _redraw_hexes(self):
         for ID in self.main_map.catalogue: 
@@ -118,6 +161,13 @@ class main_window(QMainWindow):
         """
         sys.exit()
 
+    def save_as(self):
+        """
+        Opens a dialog to accept a filename from the user, then calls the save_map function
+        """
+        self.file_name = QFileDialog.getSaveFileName(None, 'Save HexMap', './saves', 'HexMaps (*.hexmap)')
+        self.save_map()
+
     def save_map(self):
         """
         Saves the map that is saved
@@ -127,68 +177,33 @@ class main_window(QMainWindow):
         else:
             save_map( self.main_map, self.file_name)
     
-    def actionAbout(self):
-        """
-        Pops open the about dialog
-        """
-        dialog = about_dialog( self )
-        dialog.setAttribute( QtCore.Qt.WA_DeleteOnClose )
-        dialog.exec_()
+class new_load_dialog(QDialog):
+    def __init__(self, parent):
+        super(new_load_dialog, self).__init__(parent)
+        self.ui = new_load_gui()
+        self.ui.setupUi(self)
 
-    def save_as(self):
-        """
-        Opens a dialog to accept a filename from the user, then calls the save_map function
-        """
-        self.file_name = QFileDialog.getSaveFileName(None, 'Save HexMap', './saves', 'HexMaps (*.hexmap)')
-        self.save_map()
+        self.ui.new_map_button.clicked.connect(self.button_new)
+        self.ui.load_map_button.clicked.connect(self.button_load)
+        self.ui.quit_button.clicked.connect(self.button_quit)
+        self.parent = parent
 
-    def action_biome_names(self):
+    def button_new(self):
         """
-        Toggles the drawing of biome names
+        Tells the parent Window to try making a new map. This will launch the New Map Interface
         """
-        state = self.ui.actionBiome_Names.isChecked()
-        self.biome_control.draw_names = state
-        self._redraw_biomes()
+        self.parent.new()
+        self.accept()
 
-    def action_biome_bord(self):
+    def button_load(self):
         """
-        Toggles the drawing of Biome borders
+        This tells the parent window to load a map. Opens the open file dialog! 
         """
-        state = self.ui.actionBiome_Borders.isChecked()
-        self.biome_control.draw_borders = state
-        self._redraw_biomes()
+        self.parent.load()
+        self.accept()
 
-    def action_county_bord(self):
-        """
-        Toggles the drawing of county borders
-        """
-        state = self.ui.actionCounty_Borders.isChecked()
-        self.county_control.draw_borders = state
-        self._redraw_counties()
-        
-    def action_county_names(self):
-        """
-        Toggles the drawing of county Names
-        """
-        state = self.ui.actionCounty_Names.isChecked()
-        self.county_control.draw_names = state
-        self._redraw_counties()
-
-    def action_towns(self):
-        """
-        Toggles the drawing of settlements on the map
-        """
-        state = self.ui.actionTowns.isChecked()
-        self.entity_control.draw_settlements = state
-        self._redraw_entities()
-
-    def action_locations(self):
-        """
-        Toggles the drawing of entities/locations on the map
-        """
-        state = self.ui.actionLocations.isChecked()
-        self.entity_control.draw_entities = state
-        self._redraw_entities()
+    def button_quit(self):
+        sys.exit()
 
 app = QApplication(sys.argv)
 app_instance = main_window()
