27b1a59c337c20525a454fc61dd34b741f5641a7
Boggle-Head <b.smithers.actual@gmail.com>
2020-05-30 13:30:46 -0500

    adding first drafts of map generation guis

---
+++ MultiHex/guis/advanced_map_generation.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'advanced_map_generation.ui'
+#
+# Created by: PyQt5 UI code generator 5.10.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class advanced_map_dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(607, 484)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.param_combo = QtWidgets.QComboBox(Dialog)
+        self.param_combo.setObjectName("param_combo")
+        self.param_combo.addItem("")
+        self.verticalLayout.addWidget(self.param_combo)
+        self.scrollArea = QtWidgets.QScrollArea(Dialog)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 587, 400))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.formLayout = QtWidgets.QFormLayout(self.scrollAreaWidgetContents)
+        self.formLayout.setObjectName("formLayout")
+        self.ex_label = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.ex_label.setObjectName("ex_label")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.ex_label)
+        self.ex_desc = QtWidgets.QLabel(self.scrollAreaWidgetContents)
+        self.ex_desc.setObjectName("ex_desc")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.FieldRole, self.ex_desc)
+        self.ex_spin = QtWidgets.QDoubleSpinBox(self.scrollAreaWidgetContents)
+        self.ex_spin.setObjectName("ex_spin")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.ex_spin)
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout.addWidget(self.scrollArea)
+        self.horizontalLayout = QtWidgets.QHBoxLayout()
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        spacerItem = QtWidgets.QSpacerItem(40, 20, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Minimum)
+        self.horizontalLayout.addItem(spacerItem)
+        self.pushButton = QtWidgets.QPushButton(Dialog)
+        self.pushButton.setMinimumSize(QtCore.QSize(200, 0))
+        self.pushButton.setObjectName("pushButton")
+        self.horizontalLayout.addWidget(self.pushButton)
+        self.verticalLayout.addLayout(self.horizontalLayout)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Advanced Generation"))
+        self.param_combo.setItemText(0, _translate("Dialog", "Mountains"))
+        self.ex_label.setText(_translate("Dialog", "Example Label"))
+        self.ex_desc.setText(_translate("Dialog", "This is where the description will go"))
+        self.pushButton.setText(_translate("Dialog", "Continue"))
+
